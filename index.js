33543934c19302cbc90a1144cca8a5aba30fb777
Pham Bao Nguyen <75587231+baonguyenpham@users.noreply.github.com>
2022-08-27 10:16:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -27,7 +27,32 @@ let trackList = [
         path: "track/meadow-pixabay.mp3",
         background: "images/meadow.png"
     },
+    {
+        title: "Stream",
+        image: "images/dark-green.png",
+        path: "track/stream-pixabay.mp3",
+        background: "images/stream.png"
+    },
+    {
+        title: "Aurora",
+        image: "images/light-green.png",
+        path: "track/space-pixabay.mp3",
+        background: "images/aurora.png"
+    },
+    {
+        title: "Dawn",
+        image: "images/brown.png",
+        path: "track/dawn-pixabay.mp3",
+        background: "images/dawn.png"
+    },
+    {
+        title: "Night",
+        image: "images/purple.png",
+        path: "track/night-pixabay.mp3",
+        background: "images/night.png"
+    },
 ]
+
 
 let trackPosition = 0
 const totalTrack = trackList.length
